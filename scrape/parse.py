1dd3476bb3bcbd4bbf9f43ac8e136eeb942a86fb
Alexander Su <alexanderytsu@gmail.com>
2018-09-22 14:21:47 +1000

    returns the objects

--- scrape/parse.py
+++ scrape/parse.py
@@ -4,9 +4,18 @@ import re
 
 class Course():
 
+    def __str__(self):
+        return "<Course>: " +self._name
+
+    def __repr__(self):
+        return self.__str__()
+
     def __init__(self, name, reqs):
         self._name = name
         self._reqs = reqs
+        self._tri1 = True
+        self._tri2 = False
+        self._tri3 = True
 
 
 def get_all_prequisites(coursename):
@@ -34,7 +43,32 @@ def get_all_prequisites(coursename):
     dependency_list = list(set(dependency_list))
     dependency_list = clean_list(dependency_list, courselist)
     dependency_list.sort()
+    dependency_list = convert_names_to_courses(dependency_list)
     return dependency_list
+
+
+def convert_names_to_courses(name_list):
+    object_list = []
+
+    courselist = []
+
+    with open("output.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            reqs = row[3]
+            courses = re.findall("[A-Z]{4}[0-9]{4}", reqs)
+            newCourse = Course(row[1], courses)
+            courselist.append(newCourse)
+    
+
+    for x in name_list:
+        course = [obj for obj in courselist if x == obj._name]       
+        assert len(course) == 1
+        course = course[0]
+        print("Course: ", course)
+        object_list.append(course)
+        
+    return object_list
 
 
 # removes courses that don't exist anymore
