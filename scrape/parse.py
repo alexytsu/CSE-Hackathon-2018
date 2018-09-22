7c8bc83908aba66918029a4b3d6e567a9bd60230
Alexander Su <alexanderytsu@gmail.com>
2018-09-22 11:01:22 +1000

    fix parser

--- scrape/parse.py
+++ scrape/parse.py
@@ -1,11 +1,23 @@
 import csv
 import re
 
+from course import Course
+
+courses = []
 
 with open("output.csv", "r") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        print("Coursename:", row[1])
         reqs = row[3]
         courses = re.findall("[A-Z]{4}[0-9]{4}", reqs)
-        print("Prequisites", courses)
+        courses.append[Course(row[1], reqs)]
+
+for course in courses:
+    print("---------------------------------------------")
+    print("Course: ", course._name)
+    print("Requisites: ", course._reqs)
+
+
+
+
+    
